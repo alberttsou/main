1ee8bc0184436fe5ebf7dd492c22a0146d634e47
Hack Reactor Students <accounts@hackreactor.com>
2016-01-07 11:12:19 -0800

    starting angular search logic

--- client/app.js
+++ client/app.js
@@ -1,1 +1,30 @@
-var app = angular.module('myApp',[])
+angular.module('emuSearch', []).config(function($routeProvider, $httpProvider){
+  $routeProvider
+    .when('/', function(){
+      templateUrl: '/pages/splash/index.html'
+    })
+    .when('/search', function(){
+      templateUrl: '/pages/search/index.html',
+      controller: ['$scope', '$http' function($scope, $http){
+        $scope.data = {}
+
+        $scope.submit = function(){
+          $http.get({
+
+
+            params:
+              {
+                distance: $scope.data.distance,
+                maxPrice: $scope.data.price,
+              }
+          })
+
+        }
+
+      }],
+      controllerAs: search
+    })
+    .otherwise({
+      redirectTo: '/'
+    })
+})
