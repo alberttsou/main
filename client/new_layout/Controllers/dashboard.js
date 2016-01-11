08a8d83d53762e0ea2b80461f64f2bc351359dc4
Eochs <slochastic@gmail.com>
2016-01-11 14:28:45 -0800

    fixed front end to match backend on getting user bids

--- client/new_layout/Controllers/dashboard.js
+++ client/new_layout/Controllers/dashboard.js
@@ -1,45 +1,46 @@
 
 app.controller('dashboardCtrl', function($scope, $http, $rootScope){
 	$scope.message = "Welcome to the Dashboard";
+	$scope.userId = "5691749973aa5549057a2269";
 	$scope.bids;
-	$scope.jobs = [
-	{
-		title: "My job title",
-		salary: 90,
-		bids : [
-			{
-				status: false,
-				bidId: 1
-			},
-			{
-				status: false,
-				bidId: 1
-			}
-		]
-	},
-	{
-		title: "My job title",
-		salary: 90,
-		bids : [
-			{
-				status: false,
-				bidId: 1
-			}
-		]
-	},
-	{
-		title: "My job title",
-		salary: 90,
-		bids : [
-			{
-				status: false,
-				bidId: 1
-			}
-		]
-	}
-	];
+	$scope.jobs;// = [
+	// {
+	// 	title: "My job title",
+	// 	salary: 90,
+	// 	bids : [
+	// 		{
+	// 			status: false,
+	// 			bidId: 1
+	// 		},
+	// 		{
+	// 			status: false,
+	// 			bidId: 1
+	// 		}
+	// 	]
+	// },
+	// {
+	// 	title: "My job title",
+	// 	salary: 90,
+	// 	bids : [
+	// 		{
+	// 			status: false,
+	// 			bidId: 1
+	// 		}
+	// 	]
+	// },
+	// {
+	// 	title: "My job title",
+	// 	salary: 90,
+	// 	bids : [
+	// 		{
+	// 			status: false,
+	// 			bidId: 1
+	// 		}
+	// 	]
+	// }
+	// ];
 	// var userId = window.localStorage.user.id;
-	$scope.userId = $rootScope.userId;//fake user
+	//$scope.userId = $rootScope.userId;//fake user
 
 	$scope.getUserBids = function () {
 		
@@ -51,16 +52,17 @@ app.controller('dashboardCtrl', function($scope, $http, $rootScope){
 	};
 	$scope.getUserJobs = function () {
 
-		$http.get('/api/posts/' + $scope.userId,{
+		$http.get('/api/posts/', {// + $scope.userId,{
 		}).
 			success(function (data) {
+				// filter jobs so that data._id === $scope.userId
 				$scope.jobs = data
 			})
 	};
-	$scope.selectBid = function(bidId){
-		$http.put('/api/bids/' + bidId, 
+	$scope.selectBid = function(jobId, bidId){
+		$http.put('/api/posts/' + jobId, 
 			{
-				data: {status:'accepted'}
+				data: {chose: bidId}
 			}
 		)
 	}
